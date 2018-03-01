8cf2ee35cd7026ce1eca5a3bd243446bb72ef66d
zinnar <zinna.robert@gmail.com>
2018-03-01 10:56:48 -0700

    Updated for plots

--- segmented_test_2_28_2018.R
+++ segmented_test_2_28_2018.R
@@ -48,17 +48,39 @@ slope(segmented.mod.or)
 fullmodel<-nls(horn.l~ y0 +((a*pronot.l^b)/(c^b+pronot.l^b)),
     data=dung,
     start=list(y0=min(dung$horn.l), a=range(dung$horn.l), b=1.09, c=0.872))
+dung$pred<-predict(fullmodel, newdata=dung)
+
 shapiro.test(residuals(fullmodel))
+
 wa.model<-nls(horn.l~ y0 +((a*pronot.l^b)/(c^b+pronot.l^b)),
               data=wa,
-              start=list(y0=min(wa$horn.l), a=range(wa$horn.l), b=1.09696, c=0.878), nls.control(warnOnly=TRUE))
+              start=list(y0=min(wa$horn.l), a=range(wa$horn.l), b=18.4, c=0.802), nls.control(warnOnly=TRUE))
 shapiro.test(residuals(wa.model))
-
+wa$pred<-predict(wa.model, wa)
+plot(wa$pronot.l, wa$pred)
 ##this one breaks and I'm not sure whyyyyy
 or.model<-nls(horn.l~ y0 +((a*pronot.l^b)/(c^b+pronot.l^b)),
               data=or,
-              start=list(y0=min(or$horn.l), a=range(or$horn.l), b=1.10221, c=0.863), nls.control(warnOnly=TRUE))
+              start=list(y0=min(or$horn.l), a=range(or$horn.l), b=18.4, c=0.802), nls.control(warnOnly=TRUE))
+or$pred<-predict(or.model, or)
+plot(or$pronot.l, or$pred)
+ggplot(or, aes(x=pronot.l, y=pred))+
+  geom_line()+
+  geom_line(data=wa, aes(x=pronot.l, y=pred), color="blue")
+
 
 shapiro.test(residuals(or.model))
 
-ks.test(residuals(segmented.mod.or), residuals(segmented.mod.wa))
+ks.test(residuals(wa.model), residuals(or.model))
+
+or$resid <- as.numeric(residuals(or.model))
+wa$resid <-as.numeric(residuals(wa.model))
+
+ggplot(or, aes(x=pronot.l, y=resid))+
+  geom_point(color="red")+
+  geom_point(data=wa, aes(x=pronot.l, y=resid), color="blue")
+
+ggplot(or, aes(x=pronot.log, y=horn.log))+
+  geom_point(color="red")+
+  geom_point(data=wa, aes(x=pronot.log, y=horn.log), color="blue")
+
